037f2abf29453715736c44b30f3d8aca2f10cbc9
Andy Chen <andy.chen121@imperial.ac.uk>
2024-11-27 18:49:07 +0000

    tabu head

--- README.md
+++ README.md
@@ -16,9 +16,10 @@ To run the project, ensure you have the following Python packages installed:
 
 - **`Tabu.py`**: Contains the implementation of the Tabu Search algorithm. The first half of the main function uses parameters γ (gamma) = 10 and L = 20, testing the results of K = 10, 100, and 1000. The second half fixes a particular K value, varying across a range of γ (gamma) and L, creating a 3D plot of total tardiness over the two parameters.
   - Printout results for different parameters are available, stored in the `printout` folder.
+    - `tabu_head.txt`: Results for the first 5 iterations.
     - `tabu_k=10.txt`: Results for K = 10.
     - `tabu_k=100.txt`: Results for K = 100.
     - `tabu_k=1000.txt`: Results for K = 1000.
-  - Run the file directly, and the two halves will both be executed. All printout texts will be updated, and a 3D plot will be drawn. User can vary K value of the second half (line 161, currently set to K=1000), as well as changing the range of γ (gamma) and L to make plot of (lines 159, 160).
+  - Run the file directly, and the two halves will both be executed. All printout texts will be updated, and a 3D plot will be drawn. User can vary K value of the second half (line 161, currently set to K=1000), as well as changing the range of γ (gamma) and L to make plot of (lines 159, 160). The best schedule for all γ (gamma) and L will be printed in terminal.
 
 ---
--- Tabu.py
+++ Tabu.py
@@ -39,15 +39,16 @@ def total_tardiness(schedule, processing_times, due_dates):
     return tardiness
 
 # Main Tabu search function
-def tabu_search(x0, processing_times, due_dates, cost_func, L=2, gamma=100, K=3):
+def tabu_search(x0, processing_times, due_dates, cost_func, L=2, gamma=100, K=3, log=False):
     # Initialization
-    tabu_list = []
+    tabu_list = deque(maxlen=L)
     best_schedule = x0[:]
     best_cost = cost_func(x0, processing_times, due_dates)
     current_schedule = x0[:]
     current_cost = best_cost
     last_swap = 0
-
+    with open("printout/tabu_head.txt", "w") as f:
+        f.write("") # clear old tabu head log
     # Iteration loop, each iteration swap two jobs in the schedule
     for k in range(1, K + 1):
 
@@ -84,27 +85,24 @@ def tabu_search(x0, processing_times, due_dates, cost_func, L=2, gamma=100, K=3)
                 current_cost = neighbor_cost
                 last_swap = i+1
                 break
+        if k <= 5:      # log tabu first 5 iterations
+            with open("printout/tabu_head.txt", "a") as f:
+                f.write(f"Iteration {k} schedule: "+str(current_schedule) + "\n")
 
-        # Update Tabu List
-        if len(tabu_list) >= L:
-            tabu_list.pop(0)
         # Update best solution
         if current_cost < best_cost:
             best_cost = current_cost
             best_schedule = current_schedule
-            # Log the current state
-            logger.write(f"---------- iteration {k} ----------\n")  
-            logger.write(f"current cost: {current_cost}\n")
-            logger.write(f"current schedule: {current_schedule} \n")
-            logger.write(f"best cost: {best_cost} \n")
-            logger.write(f"Tabu list: {tabu_list} \n")
+            if log:
+                # Log the current state
+                logger.write(f"---------- iteration {k} ----------\n")  
+                logger.write(f"current cost: {current_cost}\n")
+                logger.write(f"current schedule: {current_schedule} \n")
+                logger.write(f"best cost: {best_cost} \n")
+                logger.write(f"Tabu list: {tabu_list} \n")
 
     return best_schedule, best_cost
 
-
-####################
-# CW Test
-####################
 
 if __name__ == "__main__":
     # Define DAG, processing times, due dates and Initial schedule
@@ -123,54 +121,41 @@ if __name__ == "__main__":
     due_dates = [172,82,18,61,93,71,217,295,290,287,253,307,279,73,355,34,233,77,88,122,71,181,340,141,209,217,256,144,307,329,269]
     x0 = [30,29,23,10,9,14,13,12,4,20,22,3,27,28,8,7,19,21,26,18,25,17,15,6,24,16,5,11,2,1,31]
 
+    ####################
+    # 1. Testing multiple K values
+    ####################
+
     for K in [10, 100, 1000]:
         logger = open(f"printout/tabu_k={K}.txt", 'w')
         logger.write(f"############## Running Tabu List with K = {K} ################# \n")
-        best_solution, best_tardiness = tabu_search(x0, processing_times, due_dates, total_tardiness, L=20, gamma=10, K=K)
+        best_solution, best_tardiness = tabu_search(x0, processing_times, due_dates, total_tardiness, L=20, gamma=10, K=K, log=True)
 
         logger.write("############## Final Results ################# \n")
         logger.write(f"Best solution with K={K}: {best_solution} \n")
         logger.write(f"Total tardiness with K={K}: {best_tardiness}\n")
 
+    ##################################################################################################
+    # 2. Ploting Diagrams over L and gamma
+    ##################################################################################################
 
-####################
-# Other Tests   
-####################
-
-# def is_valid_schedule(schedule, G):
-#     # Check if the schedule respects the precedence constraints (DAG)
-#     for i, j in edges:
-#         if schedule.index(i + 1) > schedule.index(j + 1):
-#             return False
-#     return True
-
-# # initial_solution = generate_initial_solution(G)
-# initial_solution = [30, 4, 3, 14, 23, 10, 9, 8, 7, 6, 22, 21, 13, 20, 19, 18, 17, 16, 29, 15, 11, 28, 27, 26, 25, 24, 12, 5, 2, 1, 31]
-# is_valid = is_valid_schedule(initial_solution, G)
-# print("Is the initial solution valid?", is_valid)
-
-
-##################################################################################################
-######### Ploting Diagram
-##################################################################################################
-
-    # Define the range of gamma and L values to test
-
-    gamma_values = range(1, 201, 10)  
-    L_values = range(1, 101, 10)     
-    K = 1000
+    gamma_values = range(1, 201, 5)  
+    L_values = range(1, 101, 5)     
+    K = 100
     # Storage for results
     results = []
+    best_num, best_tardiness, best_solution = 0, None, None
+    comb_num = 0
     for gamma in gamma_values:
         for L in L_values:
             schedule, tardiness = tabu_search(x0, processing_times, due_dates, total_tardiness, L, gamma, K)
+            if best_tardiness is None or tardiness < best_tardiness:
+                best_tardiness = tardiness
+                best_solution = schedule
+                best_num = comb_num
             results.append((L, gamma, tardiness))
-
+            comb_num += 1
+    best_L, best_gamma, best_tardiness = results.pop(best_num)
     # Convert results to a 2D array for plotting
-    L_gamma_tardiness = np.array(results, dtype=int)
-    # Find the best combination and remove from all results
-    opt_index = np.argmin(L_gamma_tardiness[:, 2])
-    best_L, best_gamma, best_tardiness = results.pop(opt_index)
     L_gamma_tardiness = np.array(results, dtype=int)
 
     # Extract L, gamma, and tardiness values except for the best
@@ -179,6 +164,7 @@ if __name__ == "__main__":
     tardiness_values = L_gamma_tardiness[:, 2]
 
     print(f"Best Combination: L={int(best_L)}, Gamma={int(best_gamma)}, Best Tardiness={int(best_tardiness)}")
+    print(f"Optimal schedule:", best_solution)
 
     # Plot the results in 3D
     fig = plt.figure(figsize=(12, 8))
